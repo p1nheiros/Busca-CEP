607996307505909778c7e8f54dbbbe4d15021bc3
Lucas Pinheiro <124714182+p1nheiros@users.noreply.github.com>
2023-03-24 20:14:31 -0300

    Update App.tsx

--- buscaCep/src/App.tsx
+++ buscaCep/src/App.tsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './style.css';
 
 function App() {
+  
+  const[input, setInput] = useState('')
+  
+  function handleSearch(){
+    alert("VALOR DO INPUT" + input)
+  }
+  
   return (
     <div className="container">
       <div className='title'>
@@ -11,9 +19,11 @@ function App() {
         <input
           type="text"
           placeholder="Digite seu CEP..."
+          value={input}
+          onChange={(e) => setInput(e.target.value) }
         />
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#FFF" />
         </button>
 
